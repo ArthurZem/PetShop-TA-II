5b37017ef717aaba319a9323429fe27daa0a278e
Arthur Zem de Souza <arthurzem1612@gmail.com>
2023-07-18 14:41:10 -0300

    Implementação AnimalService e Controller

--- src/application/AnimalService.ts
+++ src/application/AnimalService.ts
@@ -1,6 +1,36 @@
 import { IAnimalRepository } from "src/interfaces/IAnimalRepository";
 import { Animal } from "src/domain/Animal";
+import { Cliente } from "src/domain/Cliente";
 
 export class AnimalService{
+    private animalRepository: IAnimalRepository;
+
+    constructor(animalRepository:IAnimalRepository){
+        this.animalRepository = animalRepository;
+    }
+
+    public async criarAnimal(id: number, nome: string, especie: string, raca: string, idade: number, dono: Cliente): Promise<Animal>{
+        const animalExistente = await this.animalRepository.findById(id);
+        if(animalExistente){
+            throw new Error('ID já cadastrado para outro cliente.')
+        }
+        // criação de instância
+        const animal = new Animal(id,nome,especie,raca,idade,dono);
+        // salvar cliente no repositório
+        await this.animalRepository.save(animal);
+
+        return animal;
+    }
+
+    public async findById(id:number): Promise<Animal | null> {
+        const animal = await this.animalRepository.findById(id);
+
+        return animal;
+    }
     
+    public async findByName(nome:string) : Promise<Animal[]>{
+        const animal = await this.animalRepository.findByNome(nome);
+
+        return animal;
+    }
 }
--- src/controllers/AnimalController.ts
+++ src/controllers/AnimalController.ts
@@ -1,2 +1,39 @@
 import { Request,Response } from "express";
 import { AnimalService } from "src/application/AnimalService";
+
+export class AnimalController {
+    private animalService: AnimalService;
+
+    constructor(animalService: AnimalService){
+        this.animalService = animalService;
+    }
+
+    public async CriarAnimal(req: Request, res: Response): Promise<void>{
+        try{
+            const { id, nome, especie, raca, idade, dono} = req.body;
+
+            const animal = await this.animalService.criarAnimal(id, nome, especie, raca, idade, dono);
+
+            res.status(201).json(animal);
+        }catch(error){
+            res.status(500).json({error: 'Erro ao criar animal.'})
+        }
+    }
+
+    public async findById(req: Request, res: Response): Promise<void>{
+        try{
+            const animalId = Number(req.params.id);
+
+            const animal = await this.animalService.findById(animalId);
+         
+            if(animal){
+                res.status(200).json(animal)
+            }
+            else{
+                res.status(404).json({error: 'Animal não encontrado.'})
+            }
+        } catch(error){
+            res.status(500).json({ error:' Erro ao tentar obter o Animal.'})
+        }
+    }
+}
