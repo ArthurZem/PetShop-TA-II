c27e059492815a553401d339b81cb96a0a88a474
Arthur Zem de Souza <arthurzem1612@gmail.com>
2023-07-18 10:43:49 -0300

    Criação de Produto em domain

---
+++ src/domain/Produto.ts
@@ -0,0 +1,15 @@
+export class Animal{
+    id:number;
+    nome: String;
+    preco: number;
+    estoque: number
+
+    constructor(id: number, nome: string, preco: number, estoque: number) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+}
+
